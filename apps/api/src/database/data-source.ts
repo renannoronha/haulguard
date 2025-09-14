015f848e82e21b518b7492656ddde83de5eb6fcc
Renan Noronha <renan.noronha@eemovelagro.com.br>
2025-09-14 17:00:41 -0300

    database cli config

--- apps/api/src/database/data-source.ts
+++ apps/api/src/database/data-source.ts
@@ -1,8 +1,8 @@
-import 'dotenv/config';
-import { DataSource } from 'typeorm';
+import "dotenv/config";
+import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
-  type: 'postgres',
+  type: "postgres",
   host: process.env.POSTGRES_HOST,
   port: process.env.POSTGRES_PORT
     ? Number(process.env.POSTGRES_PORT)
@@ -11,7 +11,7 @@ export const AppDataSource = new DataSource({
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   // ATENÇÃO: para gerar migrações aponte p/ .ts; para rodar em prod (build) use .js
-  entities: ['src/**/*.entity.ts'],
-  migrations: ['src/database/migrations/*.ts'],
+  entities: ["src/**/*.entity.ts"],
+  migrations: ["src/database/migrations/*.ts"],
   synchronize: false, // SEMPRE false quando usando migrações
 });
