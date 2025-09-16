a43f8602fea1c6bcc4008df910655d8e94826b33
Renan Noronha <renancnoronha@gmail.com>
2025-09-16 11:33:59 -0300

    fix: register mongo in worker

--- src/pubsub/consumer.module.ts
+++ src/pubsub/consumer.module.ts
@@ -2,9 +2,25 @@ import { Module } from "@nestjs/common";
 import { AuditModule } from "../audit/audit.module";
 import { ConsumerService } from "./consumer.service";
 import { AppConfigModule } from "src/config/app-config.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import { AppConfigService } from "src/config/app-config.service";
 
 @Module({
-  imports: [AuditModule, AppConfigModule],
+  imports: [
+    AppConfigModule,
+    MongooseModule.forRootAsync({
+      imports: [AppConfigModule],
+      inject: [AppConfigService],
+      useFactory: (config: AppConfigService) => {
+        const mongo = config.mongo();
+        return {
+          uri: mongo.uri,
+          dbName: mongo.dbName,
+        };
+      },
+    }),
+    AuditModule,
+  ],
   providers: [ConsumerService],
 })
 export class ConsumerModule {}
